aa792e40edc4fa1f5360122a4ccdc289753e9e83
Kazi Saif Hossain Emon <133599101+kazisaif23@users.noreply.github.com>
2025-08-17 06:25:19 +0600

    Create Q8.asm
    lab 23

---
+++ Q8.asm
@@ -0,0 +1,36 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+      text db "enter the number: $"
+      text2 db 10,13,"Factorial is: $"
+.CODE
+MAIN PROC
+    MOV AX, @DATA  
+    MOV DS, AX 
+      
+    mov ah,09
+    lea dx,text
+    int 21h 
+    
+    mov ah,01
+    int 21h
+    sub al,30h
+    mov bl,al  
+    
+    mov ah,09
+    lea dx,text2
+    int 21h
+    
+    mov ax,1
+    fact:
+    mul bl
+    dec bl
+    jnz fact
+    add al,30h
+    mov dl,al
+    mov ah,02
+    int 21h
+    mov ah,4ch
+    int 21h
+    main endp
+end main
